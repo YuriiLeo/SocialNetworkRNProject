b7ff714e051ac9fddf0a1939bd15bbc178d61702
YuriiLeo <yuriileontenko@gmail.com>
2023-02-21 22:28:13 +0200

    add fonts & new files

--- App.js
+++ App.js
@@ -7,25 +7,36 @@ import {
   Text,
   View,
   Button,
+  Dimensions,
 } from "react-native";
-import Registration from "./src/Screens/RegistrationScreen";
-import Login from "./src/Screens/LoginScreen";
-import { useCallback, useState } from "react";
+import Registration from "./src/Screens/auth/RegistrationScreen";
+import Login from "./src/Screens/auth/LoginScreen";
+import { useCallback, useEffect, useState } from "react";
 
 import { useFonts } from "expo-font";
 import * as SplashScreen from "expo-splash-screen";
 
 SplashScreen.preventAutoHideAsync();
-// const loadFonts = async () => {
-//   await Font.loadAsync({
-//     Gajraj: require("./assets/fonts/GajrajOne-Regular.ttf"),
-//   });
-// };
 
 export default function App() {
   const [fontsLoaded] = useFonts({
     Inter: require("./assets/fonts/GajrajOne-Regular.ttf"),
   });
+
+  const [dimensions, setDimensions] = useState(
+    Dimensions.get("window").width - 16 * 2
+  );
+  useEffect(() => {
+    const onChange = () => {
+      const width = Dimensions.get("window").width - 16 * 2;
+      console.log("width", width);
+      setDimensions(width);
+    };
+    Dimensions.addEventListener("change", onChange);
+    return () => {
+      Dimensions.removeEventListener("change", onChange);
+    };
+  }, []);
 
   const onLayoutRootView = useCallback(async () => {
     if (fontsLoaded) {
@@ -43,7 +54,11 @@ export default function App() {
     >
       <ImageBackground
         source={require("./assets/images/PhotoBG.jpg")}
-        style={styles.image}
+        // style={styles.image}
+        style={{
+          ...styles.image,
+          width: dimensions,
+        }}
       >
         {/* <Registration /> */}
         <Login />
