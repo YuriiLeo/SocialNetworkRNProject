f83974bb6d0a3de01cc4017e890750761aa2ac9f
YuriiLeo <yuriileontenko@gmail.com>
2023-03-22 23:50:47 +0000

    add coments

--- src/Screens/nestedScreens/CommentsScreen.js
+++ src/Screens/nestedScreens/CommentsScreen.js
@@ -15,63 +15,27 @@ import {
   View,
   Dimensions,
   FlatList,
+  KeyboardAvoidingView,
 } from "react-native";
 import { TextInput } from "react-native-gesture-handler";
 import { useSelector } from "react-redux";
 import { db } from "../../firebase/config";
 import { AntDesign } from "@expo/vector-icons";
+import CommentsItem from "../../components/CommentsItem";
 
-const Item = ({ item }) => {
-  const getData = () => {
-    const date = new Date(item.createdAt?.seconds * 1000);
-    const options = {
-      day: "numeric",
-      year: "numeric",
-      month: "long",
-      hour: "numeric",
-      minute: "numeric",
-      hour12: false,
-    };
-    const dateFormatter = new Intl.DateTimeFormat(
-      "en-us",
-      options
-    );
-    const currentData = dateFormatter.format(date);
-
-    return currentData;
-  };
-
-  const currentCommentData = getData();
-
-  return (
-    <View style={styles.itemComments}>
-      <View style={styles.imageWrap}>
-        <Image
-          style={styles.imageAvatar}
-          // source={
-          //   item.avatar { uri: item.avatar }
-          // }
-        />
-      </View>
-
-      <View style={styles.textCommentsWrap}>
-        <Text style={styles.text}>{item.comment}</Text>
-        <Text style={styles.title}>
-          {currentCommentData}
-        </Text>
-      </View>
-    </View>
-  );
-};
 export default function CommentsScreen({
   route,
   navigation,
 }) {
   const [comment, setComment] = useState("");
   const [allComments, setAllComments] = useState([]);
+  const [isShowKeyBoard, setIsShowKeyBoard] =
+    useState(false);
 
   const { photo, postId } = route.params;
-  const { login } = useSelector((state) => state.auth);
+  const { login, avatar } = useSelector(
+    (state) => state.auth
+  );
 
   const createPost = async () => {
     try {
@@ -81,6 +45,7 @@ export default function CommentsScreen({
         {
           comment,
           login,
+          avatar,
           createdAt: serverTimestamp(),
         }
       );
@@ -114,74 +79,82 @@ export default function CommentsScreen({
   };
 
   return (
-    <View style={styles.container}>
-      <View
-        style={{
-          backgroundColo: "#FFF",
-          justifyContent: "flex-end",
-        }}
-      >
-        <View style={styles.takePhotoContainer}>
-          <Image
-            source={{ uri: photo }}
-            style={{
-              width: 380,
-              // Dimensions.get("window").width - 32,
-              height: 240,
-              borderRadius: 8,
-            }}
-          />
-        </View>
-
-        {allComments.length !== 0 ? (
-          <View
-            style={{
-              height: 270,
-              marginBottom: 20,
-            }}
-          >
-            <FlatList
-              data={allComments}
-              renderItem={({ item }) => (
-                <Item item={item} />
-              )}
-              keyExtractor={(item) => item.id}
+    <KeyboardAvoidingView
+      behavior={
+        Platform.OS === "ios" ? "padding" : "height"
+      }
+      style={{ flex: 1, backgroundColor: "#FFFFFF" }}
+    >
+      <View style={styles.container}>
+        <View
+          style={{
+            backgroundColo: "#FFF",
+            justifyContent: "flex-end",
+          }}
+        >
+          <View style={styles.takePhotoContainer}>
+            <Image
+              source={{ uri: photo }}
+              style={{
+                width: 380,
+                // Dimensions.get("window").width - 32,
+                height: isShowKeyBoard ? 0 : 240,
+                borderRadius: 8,
+              }}
             />
           </View>
-        ) : (
-          <Text
-            style={{
-              fontSize: 16,
-              marginTop: 50,
-              textAlign: "center",
-              fontWeight: "700",
-              fontFamily: "SS_Bold",
-            }}
-          >
-            Not comments
-          </Text>
-        )}
-        <View style={styles.commentPublishContainer}>
-          <TextInput
-            style={styles.input}
-            placeholder={"Comment..."}
-            // onFocus={() => setIsShowKeyBoard(true)}
-            value={comment}
-            onChangeText={(value) => setComment(value)}
-          />
-          <TouchableOpacity
-            onPress={SubmitComment}
-            style={styles.publishBtn}
-          >
-            <AntDesign
-              name="arrowup"
-              size={20}
-              color="#FFFFFF"
+
+          {allComments.length !== 0 ? (
+            <View
+              style={{
+                height: isShowKeyBoard ? 100 : 270,
+                marginBottom: 20,
+              }}
+            >
+              <FlatList
+                data={allComments}
+                renderItem={({ item }) => (
+                  <CommentsItem item={item} />
+                )}
+                keyExtractor={(item) => item.id}
+              />
+            </View>
+          ) : (
+            <Text
+              style={{
+                fontSize: 16,
+                marginTop: 50,
+                textAlign: "center",
+                fontWeight: "700",
+                fontFamily: "SS_Bold",
+              }}
+            >
+              Not comments
+            </Text>
+          )}
+          <View style={styles.commentPublishContainer}>
+            <TextInput
+              style={styles.input}
+              placeholder={"Comment..."}
+              onFocus={() => setIsShowKeyBoard(true)}
+              onBlur={() => setIsShowKeyBoard(false)}
+              value={comment}
+              onChangeText={(value) => setComment(value)}
             />
-          </TouchableOpacity>
+            <TouchableOpacity
+              onPress={SubmitComment}
+              style={styles.publishBtn}
+            >
+              <AntDesign
+                name="arrowup"
+                size={20}
+                color="#FFFFFF"
+              />
+            </TouchableOpacity>
+          </View>
         </View>
       </View>
-    </View>
+    </KeyboardAvoidingView>
   );
 }
 
@@ -192,24 +165,14 @@ const styles = StyleSheet.create({
     backgroundColor: "#ffffff",
   },
   takePhotoContainer: {
-    // position: "absolute",
-    // borderColor: "#fff",
-    // borderWidth: 1,
-    // height: "100%",
-    // width: "100%",
-    // marginVertical: 32,
     marginTop: 32,
     marginBottom: 32,
   },
   commentPublishContainer: {
-    // justifyContent: "center",
-    // alignItems: "center",
     position: "relative",
     justifyContent: "center",
   },
   input: {
-    // bottom: 16,
-    // right: 16,
     marginTop: 16,
     paddingLeft: 16,
     borderWidth: 1,
@@ -237,43 +200,5 @@ const styles = StyleSheet.create({
     padding: 20,
     marginVertical: 8,
     marginHorizontal: 16,
-  },
-  title: {
-    // justifyContent: "flex-end",
-    // alignItems: "flex-end",
-    textAlign: "right",
-    fontSize: 10,
-    color: "#BDBDBD",
-    fontFamily: "SS_Regular",
-  },
-  itemComments: {
-    flexDirection: "row",
-    marginBottom: 32,
-  },
-  imageWrap: {
-    marginRight: 16,
-  },
-  imageAvatar: {
-    width: 28,
-    height: 28,
-    borderRadius: 50,
-    borderWidth: 1,
-    borderColor: "#BDBDBD",
-    backgroundColor: "grey",
-  },
-  textCommentsWrap: {
-    padding: 16,
-    backgroundColor: "rgba(0, 0, 0, 0.03)",
-    borderBottomLeftRadius: 8,
-    borderBottomRightRadius: 8,
-    borderTopRightRadius: 8,
-  },
-  text: {
-    marginBottom: 8,
-    width: 267,
-    fontFamily: "SS_Regular",
-    fontSize: 13,
-    lineHeight: 18,
-    color: "#212121",
   },
 });
---
+++ src/components/CommentsItem.js
@@ -0,0 +1,92 @@
+import React from "react";
+import {
+  Image,
+  StyleSheet,
+  Text,
+  View,
+} from "react-native";
+
+export default function CommentsItem({ item }) {
+  // const getData = () => {
+  //   const date = new Date(item.createdAt?.seconds * 1000);
+  //   console.log("date", date);
+
+  //   const options = {
+  //     day: "numeric",
+  //     year: "numeric",
+  //     month: "long",
+  //     hour: "numeric",
+  //     minute: "numeric",
+  //     hour12: false,
+  //   };
+  //   const dateFormatter = new Intl.DateTimeFormat(
+  //     "en-us",
+  //     options
+  //   );
+  //   const currentData = dateFormatter.format(date);
+  //   console.log("currentData", currentData);
+  //   return currentData;
+  // };
+  const formatedDate = new Date(
+    item.createdAt?.seconds * 1000
+  ).toLocaleString();
+
+  // const formatedDate = getData();
+
+  return (
+    <View style={styles.itemComments}>
+      <View style={styles.imageWrap}>
+        <Image
+          style={styles.imageAvatar}
+          source={{ uri: item.avatar }}
+        />
+      </View>
+
+      <View style={styles.textCommentsWrap}>
+        <Text style={styles.text}>{item.comment}</Text>
+        <Text style={styles.title}>{formatedDate}</Text>
+      </View>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  title: {
+    // justifyContent: "flex-end",
+    // alignItems: "flex-end",
+    textAlign: "right",
+    fontSize: 10,
+    color: "#BDBDBD",
+    fontFamily: "SS_Regular",
+  },
+  itemComments: {
+    flexDirection: "row",
+    marginBottom: 32,
+  },
+  imageWrap: {
+    marginRight: 16,
+  },
+  imageAvatar: {
+    width: 28,
+    height: 28,
+    borderRadius: 50,
+    borderWidth: 1,
+    borderColor: "#BDBDBD",
+    backgroundColor: "grey",
+  },
+  textCommentsWrap: {
+    padding: 16,
+    backgroundColor: "rgba(0, 0, 0, 0.03)",
+    borderBottomLeftRadius: 8,
+    borderBottomRightRadius: 8,
+    borderTopRightRadius: 8,
+  },
+  text: {
+    marginBottom: 8,
+    width: 267,
+    fontFamily: "SS_Regular",
+    fontSize: 13,
+    lineHeight: 18,
+    color: "#212121",
+  },
+});
